85d27c31fdcacc897824c0252fcc12f261508bc0
pranjalsrajput <pranjalsrajput@gmail.com>
2019-12-20 00:57:25 +0100

    updated

--- VideoAndImageGrid/FaceBlurring.py
+++ VideoAndImageGrid/FaceBlurring.py
@@ -1,23 +1,7 @@
-# Python Program to blur image
-
-# Importing cv2 module
 import cv2
 
-# bat.jpg is the batman image.
 img = cv2.imread('/home/pranjal/Documents/Assignments/Deep Learning Project/NewDataset/Videos For Sample/35/Frames/VID_20191013_131503695_trimmed_8.jpg')
 
-
-
-# make sure that you have saved it in the same folder
-# Averaging
-# You can change the kernel size as you want
-# avging = cv2.blur(img, (10, 10))
-# cv2.imshow('Averaging', avging)
-# cv2.imwrite('/home/pranjal/Documents/Assignments/Deep Learning Project/NewDataset/Videos For Sample/35/Frames/testavging.jpg',avging)
-# cv2.waitKey(0)
-
-# Gaussian Blurring
-# Again, you can change the kernel size
 gausBlur = cv2.GaussianBlur(img, (9, 9), 0)
 cv2.imshow('Gaussian Blurring', gausBlur)
 cv2.imwrite('/home/pranjal/Documents/Assignments/Deep Learning Project/NewDataset/Videos For Sample/35/Frames/testCompress1.jpg',gausBlur,[cv2.IMWRITE_JPEG_QUALITY, 59])
